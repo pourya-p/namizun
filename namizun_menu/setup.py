c4b04f63a0fce3e18d134ab7da79a8cc430c4b8f
pourya <pourya.parhoode@gmail.com>
2026-05-29 09:29:37 +0000

    fix setuptools compatibility for flat-layout modules

--- namizun_menu/setup.py
+++ namizun_menu/setup.py
@@ -4,10 +4,10 @@ setup(name='namizun_menu',
       version='1.0.0',
       description='namizun menu',
       author='MalKeMit',
-      author_email='khodemalkemit@gmail.com',
-      url='https://github.com/malkemit/namizun',
+      url='https://github.com/pourya-p/namizun.git',
       setup_requires=['wheel'],
       install_requires=['colored~=1.4.4',
                         'pyfiglet~=0.8.post1',
-                        'prettytable~=3.5.0']
+                        'prettytable~=3.5.0'],
+      py_modules=["monitor", "udp_submenu", "network_submenu", "display", "main_menu"],
       )
