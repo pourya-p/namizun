f38814d7a4c1f8e7ba884a053aa7c03f1c3836e8
pourya <53395528+pourya-p@users.noreply.github.com>
2026-07-04 12:05:59 +0330

    Update pyfiglet for Python 3.12 compatibility

--- namizun_menu/setup.py
+++ namizun_menu/setup.py
@@ -7,7 +7,7 @@ setup(name='namizun_menu',
       url='https://github.com/pourya-p/namizun.git',
       setup_requires=['wheel'],
       install_requires=['colored~=1.4.4',
-                        'pyfiglet~=0.8.post1',
+                        'pyfiglet>=1.0.4,<2',
                         'prettytable~=3.5.0'],
       py_modules=["monitor", "udp_submenu", "network_submenu", "display", "main_menu"],
       )
